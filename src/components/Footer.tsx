a225cbea5240abf14105e69bc010cf92827b01c1
João Victor de Carvalho Montenegro <joritodev@gmail.com>
2025-12-02 14:44:54 -0300

    #

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Heart, Facebook, Instagram, Mail, MapPin, Phone } from "lucide-react";
+import { Facebook, Instagram, Mail, MapPin, Phone } from "lucide-react";
 import { useScrollToSection } from "../hooks/useScrollToSection";
 import {
   ORG_NAME,
